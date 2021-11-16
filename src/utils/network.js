c8b0f591e9e19269a88162cf54b6fb3c3e61a186
Alexander Nikolaev <quattro.ex@gmail.com>
2021-11-16 08:59:43 +0300

    rename index.js to index.tsx

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,9 +1,8 @@
-export const getData = async (number) => {
+export const getData = async telNumber => {
   // const API_KEY = process.env.REACT_APP_API_KEY;
-  const url = `http://apilayer.net/api/validate?access_key=85b00c75c027720450d2e7659659bee8&number=${number}&country_code=RU`;
+  const url = `http://apilayer.net/api/validate?access_key=85b00c75c027720450d2e7659659bee8&number=${telNumber}&country_code=RU`;
 
   const res = await fetch(url);
-
   if (!res.ok) {
     throw new Error(res.status);
   }
